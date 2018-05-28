f9ce2b4a545bfeac5728deee98c7a0ad88feac19
Vincent Picone <Vincent.Patrick.Picone@ibm.com>
2018-05-28 14:27:52 -0500

    Update footer.

--- Homepage/Footer.js
+++ Homepage/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <Segment textAlign="center" inverted style={{ padding: '3em 0em', margin: '0' }}>
     <Container>
       <Header inverted as="h4">
-        Website by <a style={{color: 'white', textDecoration: 'underline'}} href="https://www.vincepic.one">Vince Picone</a>
+        Website by <a style={{color: 'white', textDecoration: 'underline'}} href="https://www.vincepicone.com">Vince Picone</a>
       </Header>
     </Container>
   </Segment>
