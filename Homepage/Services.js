c947a03af5aab4051ae67bb73b234d0cb3b94ec0
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-03 22:39:21 -0600

    chore: update prices

--- Homepage/Services.js
+++ Homepage/Services.js
@@ -10,7 +10,7 @@ const Services = () => (
         <Table.Body>
           <Table.Row>
             <Table.Cell>Standard Cut</Table.Cell>
-            <Table.Cell>$28</Table.Cell>
+            <Table.Cell>$32</Table.Cell>
             <Table.Cell>
               Stylist will preform a short hair cut using clippers and scissors
             </Table.Cell>
@@ -18,24 +18,23 @@ const Services = () => (
           <Table.Row>
             <Table.Cell>Buzz Cut</Table.Cell>
             <Table.Cell>$20</Table.Cell>
-            <Table.Cell>A single clipper guard all over</Table.Cell>
+            <Table.Cell>A single clipper guard all over.</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Spruce up w/o razor</Table.Cell>
             <Table.Cell>$20</Table.Cell>
             <Table.Cell>
-              We incorporate a combination of clippers and scissors to shape up
-              your beard with or with a razor
+              We incorporate a combination of clippers and scissors to shape up your beard with or with a razor.
             </Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Cut & Beard</Table.Cell>
-            <Table.Cell>$45</Table.Cell>
-            <Table.Cell>The Standard with a Spruce Up beard trim</Table.Cell>
+            <Table.Cell>$53</Table.Cell>
+            <Table.Cell>The standard and The spruce up. $5 off if a razor isn't used on a beard</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>The Old Fashioned</Table.Cell>
-            <Table.Cell>$35</Table.Cell>
+            <Table.Cell>$40</Table.Cell>
             <Table.Cell>
               Includes hot lather, essential oils and a three-step hot towel
               process to ensure the cleanest shave
